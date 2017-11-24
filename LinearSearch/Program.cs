b38d8c06b41b5cad11545959f02acc2917ee2449
veekayr <vardhireddy@live.co.uk>
2017-11-24 18:19:23 +0800

    Added Linear Search

---
+++ LinearSearch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearch
+{
+    class Program
+    {
+        public static int LinearSearch(int[] arr, int ele)
+        {
+            for(int i = 0;i<arr.Length;i++)
+            {
+                if(arr[i] == ele)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 10 };
+            int ele = 10;
+            Console.WriteLine("{0} found at index {1}", ele, LinearSearch(arr, ele));
+            Console.Read();
+        }
+    }
+}
