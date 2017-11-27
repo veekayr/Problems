cf45c00ec859dbbf1faea5d9868754d62df266ec
Venkata Vardhi Reddy (Beyondsoft) <v-redv@microsoft.com>
2017-11-27 16:51:44 +0800

    Added Prime Number Solution

---
+++ PrimeNumberRange/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberRange
+{
+    class Program
+    {
+        public static void SieveofEratosthenes(int n, List<int> primes)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                primes.Add(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = 50;
+            List<int> primes = new List<int>();
+            SieveofEratosthenes(n, primes);
+            Console.WriteLine(String.Join(",", primes));
+            Console.Read();
+        }
+    }
+}
